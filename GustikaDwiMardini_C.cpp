4af8028d9a30b57851948348b3276469e1388f26
gustika17 <112964518+gustika17@users.noreply.github.com>
2022-09-08 14:10:47 +0700

    Add files via upload

---
+++ GustikaDwiMardini_C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x = 15;
+	int y = 2;
+	
+	cout << x + y << endl;
+	cout << x - y << endl;
+	cout << x * y << endl;
+	cout << x / y << endl;
+	cout << x % y << endl;
+	
+	return 0;
+}
