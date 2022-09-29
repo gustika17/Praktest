6a5e7cbd092800a2213871c92404a2ef6067e4df
gustika17 <112964518+gustika17@users.noreply.github.com>
2022-09-29 13:16:51 +0700

    Add files via upload

---
+++ 2257051004_GustikaDwiMardini_Praktest3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int a;
+	int b;
+	
+	cin >> a;
+	cin >> b;
+	
+	for (int i =a; i<=b; i+=a){
+			cout << i << endl;
+		}
+		return 0;
+	}
+
